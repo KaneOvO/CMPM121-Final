de94dc2ed39d956b7651323de086929a798ea77a
nmjxblw <zchen386@ucsc.edu>
2023-11-22 12:01:51 -0800

    mod pool cs

--- Assets/Scripts/Managers/PoolManager.cs
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -4,15 +4,74 @@ using UnityEngine;
 
 public class PoolManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField] Pool[] projectilePools;
+    static Dictionary<GameObject, Pool> dictionary;
     void Start()
     {
-        
+        dictionary = new Dictionary<GameObject, Pool>();
+        Initialize(projectilePools);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Initialize(Pool[] pools)
     {
-        
+        foreach (var pool in pools)
+        {
+#if UNITY_EDITOR
+            //skip if key has already been added
+            if (dictionary.ContainsKey(pool.Prefab)) {
+                Debug.LogWarning("Pool key already exists: " + pool.Prefab.name);
+                continue;
+            }
+#endif
+            dictionary.Add(pool.Prefab, pool);
+            Transform poolParent = new GameObject("Pool:" + pool.Prefab.name).transform;
+            poolParent.parent = transform;
+            pool.Initialize(poolParent);
+        }
+    }
+
+    public static GameObject Release(GameObject prefab)
+    {
+#if UNIIY_EDITOR
+        if(!dictionary.ContainsKey(prefab)){
+        Debug.LogError(string.Format("No pool exists for prefab:", prefab.name));
+            reutrn null;
+        }
+#endif
+        return dictionary[prefab].PrepareObject();
+    }
+
+    public static GameObject Release(GameObject prefab, Vector3 pos)
+    {
+#if UNIIY_EDITOR
+        if(!dictionary.ContainsKey(prefab)){
+        Debug.LogError(string.Format("No pool exists for prefab:", prefab.name));
+            reutrn null;
+        }
+#endif
+        return dictionary[prefab].PrepareObject(pos);
+    }
+
+    public static GameObject Release(GameObject prefab, Vector3 pos, Quaternion rot)
+    {
+#if UNIIY_EDITOR
+        if(!dictionary.ContainsKey(prefab)){
+        Debug.LogError(string.Format("No pool exists for prefab:", prefab.name));
+            reutrn null;
+        }
+#endif
+        return dictionary[prefab].PrepareObject(pos, rot);
+    }
+
+    public static GameObject Release(GameObject prefab, Vector3 pos, Quaternion rot, Vector3 localScale)
+    {
+#if UNIIY_EDITOR
+        if(!dictionary.ContainsKey(prefab)){
+        Debug.LogError(string.Format("No pool exists for prefab:", prefab.name));
+            reutrn null;
+        }
+#endif
+        return dictionary[prefab].PrepareObject(pos, rot, localScale);
     }
 }
