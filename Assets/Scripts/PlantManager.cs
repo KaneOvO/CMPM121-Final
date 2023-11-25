486b971b1676630bcb3ee4d01bcb0e0291308117
Zexuan <lzx213@sina.com>
2023-11-24 18:11:48 -0800

    add plant logic

--- Assets/Scripts/PlantManager.cs
+++ Assets/Scripts/PlantManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlantManager : MonoBehaviour
 {
@@ -9,6 +11,9 @@ public class PlantManager : MonoBehaviour
     public int numOfCarrot;
     public int numOfCabbage;
     public int numOfOnion;
+
+    public Tilemap landTilemap;
+    public Tilemap growingZoneTilemap;
 
     void Awake()
     {
@@ -24,7 +29,7 @@ public class PlantManager : MonoBehaviour
         }
     }
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,9 +42,46 @@ public class PlantManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (plantSelected != 0)
+        {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int cellPos = landTilemap.WorldToCell(worldPos);
 
+            if (CanPlantSeed(cellPos))
+            {
+                PlantSeed(cellPos);
+            }
+        }
     }
 
+    bool CanPlantSeed(Vector3Int position)
+    {
+        bool landTileExists = landTilemap.GetTile(position) != null;
+        bool growingZoneTileExists = growingZoneTilemap.GetTile(position) != null;
 
+        return landTileExists && !growingZoneTileExists;
+    }
 
+    void PlantSeed(Vector3Int position)
+    {
+        Debug.Log("Planting seed at " + position);
+        Debug.Log("Planting seed type " + plantSelected);
+        switch (plantSelected)
+        {
+            case 1:
+                growingZoneTilemap.SetTile(position, Resources.Load<Tile>("Tiles/Carrot"));
+                break;
+            case 2:
+                growingZoneTilemap.SetTile(position, Resources.Load<Tile>("Tiles/Cabbage"));
+                break;
+            case 3:
+                growingZoneTilemap.SetTile(position, Resources.Load<Tile>("Tiles/Onion"));
+                break;
+        }
+    }
+
+    void plantInformation(Vector3Int position )
+    {
+        
+    }
 }
