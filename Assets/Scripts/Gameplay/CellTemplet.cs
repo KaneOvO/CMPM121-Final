d1335460a6a34127baeb97e420737d7155465dfc
nmjxblw <zchen386@ucsc.edu>
2023-11-24 17:03:00 -0800

    mod templet and resourses

--- Assets/Scripts/Gameplay/CellTemplet.cs
+++ Assets/Scripts/Gameplay/CellTemplet.cs
@@ -1,22 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class cell
+public class Cell
 {
     public Vector3 pos;
     public float water;
     public bool isLand;
     public plantType plant;
     public int level;
-
-    public float getSun()
-    {
-        float sun = pos.x * pos.y * GameManager.Instance.currentTurn;
-        return sun;
-    }
     public void grow(float growUp)
     {
-        if (water * getSun() * nearByPlants() >= growUp)
+        if (water * RandomResources.GetSun(this) * nearByPlants() >= growUp)
         {
             level++;
         }
